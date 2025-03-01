f85b90f2447edce86bc1be94986e99c11951bd98
Jayraj1508 <106257611+Jayraj1508@users.noreply.github.com>
2025-03-01 14:29:14 +0530

    added helloword

---
+++ src/test/java/com/springproject/MyfirstProject/MyfirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.MyfirstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
